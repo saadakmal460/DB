c766f1a37e6f423350049e951ba2c662cda3131b
saadakmal460 <saadakmal460@gmail.com>
2024-02-02 18:40:44 +0500

    Manual Tasks

---
+++ Week 3/HomeTask.sql
@@ -0,0 +1,14 @@
+
+--1st--
+--SELECT OrderID FROM Orders WHERE ShippedDate > RequiredDate;
+
+
+--2nd--
+--Select Country from Employees--;
+
+
+--3rd--
+--Select FirstName from Employees where ReportsTo Is NULL--;
+
+
+
---
+++ Week 3/LabTask.sql
@@ -0,0 +1,11 @@
+--Select RegNo , FirstName , LastName , GPA , Contact FROM Studnet;
+
+--Select RegNo FROM  Studnet
+
+--Select RegNo , FirstName , LastName , GPA , Contact FROM Studnet WHERE GPA>3.5;
+
+--Select RegNo , FirstName , LastName , GPA , Contact FROM Studnet WHERE GPA<=3.5;
+
+--Select FirstName , ' ' ,LastName FROM Studnet;
+
+Select * FROM Orders where ID=1;
